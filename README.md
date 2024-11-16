0e9dc524b5dec3d676bc65f2d64195359785b1bc
Mor <mortogo321@gmail.com>
2024-11-16 13:03:25 +0700

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# NestJS Fastify Microservices
+- NestJS + Fastify
+- PostgeSQL
+- RabbitMQ
+
+## Required
+- NetsJS CLI: https://docs.nestjs.com/cli/overview  
+- dotenvx: https://github.com/dotenvx/dotenvx
+
+```bash
+yarn global add @nestjs/cli
+yarn global add @dotenvx/dotenvx
+```
+
+## Scripts
+```bash
+# prisma command
+yarn prisma:dev generate
+yarn prisma:dev migrate dev --name init
+yarn prisma:dev db push
+```
+
+## Environment Variables
+Global: `.env.{dev | prod}`  
+Service: `apps/{service-name}/.env.{dev | prod}`  
+
+## Services
+api - http://localhost:8000  
+auth - http://localhost:8001  
+alert - http://localhost:8002  
+
+## Management Tools
+RabbitMQ: http://localhost:15672  
+```
+u: admin
+p: Password@01
+```
+
+MailHog: http://localhost:8025  
+
+## Documents
+http://localhost:{service-port}/docs  
---
+++ server/apps/api/src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ApiModule } from './api.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    ApiModule,
+    new FastifyAdapter(),
+  );
+  const appName = process.env.APP_NAME;
+  const port = process.env.PORT;
+  console.log(process.env.PORT);
+  const documentConfig = new DocumentBuilder()
+    .setTitle(`${appName} Service`)
+    .setDescription(`${appName} Service API description`)
+    .setVersion('1.0')
+    .addTag(appName)
+    .build();
+  const document = SwaggerModule.createDocument(app, documentConfig);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port, '0.0.0.0');
+  console.log(`${appName} is running on ${port}`);
+}
+bootstrap();
