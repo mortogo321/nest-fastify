619af65f9ac66c91944d5a826a9d8cf04cb9a8dd
Mor <mortogo321@gmail.com>
2024-11-16 13:21:28 +0700

    add auth app

--- server/apps/auth/src/auth.module.ts
+++ server/apps/auth/src/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/apps/auth/.env.app`,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- server/apps/auth/src/main.ts
+++ server/apps/auth/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AuthModule,
+    new FastifyAdapter(),
+  );
+  const appName = process.env.APP_NAME;
+  const port = process.env.PORT;
+  console.log(process.env.PORT);
+  const documentConfig = new DocumentBuilder()
+    .setTitle(`${appName} Service`)
+    .setDescription(`${appName} Service API description`)
+    .setVersion('1.0')
+    .addTag(appName)
+    .build();
+  const document = SwaggerModule.createDocument(app, documentConfig);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port, '0.0.0.0');
+  console.log(`${appName} is running on ${port}`);
 }
 bootstrap();
