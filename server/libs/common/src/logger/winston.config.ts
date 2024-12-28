981144f9bfcdcc9f242b1de5bc40d5409e8199ec
Mor <mortogo321@gmail.com>
2024-12-28 11:38:41 +0700

    winston config

--- server/libs/common/src/logger/winston.config.ts
+++ server/libs/common/src/logger/winston.config.ts
@@ -1,13 +1,28 @@
 import { format, transports } from 'winston';
 
+const logFile = [
+  new transports.File({
+    format: format.combine(
+      format.label({ label: process.env.APP_NAME }),
+      format.timestamp(),
+      format.colorize(),
+      format.simple(),
+    ),
+    filename: '/logs/error.log',
+    level: 'error',
+  }),
+];
+
+const logConsole = [
+  new transports.Console({
+    format: format.combine(
+      format.timestamp(),
+      format.colorize(),
+      format.simple(),
+    ),
+  }),
+];
+
 export const winstonConfig = {
-  transports: [
-    new transports.Console({
-      format: format.combine(
-        format.timestamp(),
-        format.colorize(),
-        format.simple(),
-      ),
-    }),
-  ],
+  transports: process.env.NODE_ENV !== 'production' ? logConsole : logFile,
 };
