a7e1355fa5d80a727a331e5d499697b567eb5f3c
Mor <mortogo321@gmail.com>
2025-01-03 12:50:16 +0700

    add duration in request log

--- server/libs/common/src/middlewares/logger.middleware.ts
+++ server/libs/common/src/middlewares/logger.middleware.ts
@@ -5,13 +5,18 @@ export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger();
 
   use(req: any, res: any, next: () => void) {
+    const startTime = new Date().getTime();
+
     res.on('finish', () => {
       const statusCode = res.statusCode;
+      const endTime = new Date().getTime();
+      const duration = endTime - startTime;
+      const log = `[${req.method}] ${req.url} - ${statusCode} - ${duration}ms`;
 
       if ([401, 403, 404, 405].includes(statusCode)) {
-        this.logger.warn(`[${req.method}] ${req.url} - ${statusCode}`);
+        this.logger.warn(log);
       } else {
-        this.logger.log(`[${req.method}] ${req.url} - ${statusCode}`);
+        this.logger.log(log);
       }
     });
 
