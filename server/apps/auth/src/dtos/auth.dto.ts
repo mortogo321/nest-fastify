96137470851f3901991fcf2006d8d0d251312cf4
Mor <mortogo321@gmail.com>
2025-01-11 10:40:08 +0700

    readonly dto

--- server/apps/auth/src/dtos/auth.dto.ts
+++ server/apps/auth/src/dtos/auth.dto.ts
@@ -9,7 +9,7 @@ export class AuthDto {
   @IsNotEmpty()
   @IsString()
   @IsEmail()
-  email: string;
+  readonly email: string;
 
   @ApiProperty({
     required: true,
@@ -17,5 +17,5 @@ export class AuthDto {
   @IsNotEmpty()
   @IsString()
   @Length(4, 20, { message: 'Password has to be at between 4 and 20 chars' })
-  password: string;
+  readonly password: string;
 }
